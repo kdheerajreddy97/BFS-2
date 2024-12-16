83faba7381295633be91c02398b2da929132d4b8
kdheerajreddy97 <kdheerajreddy97@gmail.com>
2024-12-16 02:35:40 -0600

    Done BFS-2

---
+++ Problem1.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        q = deque([root])
+        if not root:
+            return []
+
+        while q:
+            rightside = None
+
+            for i in range(len(q)):
+                node = q.popleft()
+
+                rightside = node
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            if rightside:
+                res.append(rightside.val)
+
+        return res
+
+
+
+
+
